2f202da61662e0856aace8a626fae617c7197ed0
Chris Gurung <chris.grg7@gmail.com>
2023-06-15 22:28:56 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Eat", completed: true },
+    { id: 2, name: "Sleep", completed: false },
+    { id: 3, name: "Code", completed: true },
+    { id: 4, name: "Repeat", completed: false },
+  ]);
+  const [newTasks, setNewTasks] = useState("");
+  const [show, setShow] = useState(true);
+  const handleAdd = () => {
+    const newT = [
+      ...tasks,
+      { name: newTasks, id: tasks.length + 1, completed: false },
+    ];
+    setTasks(newT);
+  };
+  const handleChange = (event) => {
+    setNewTasks(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Todo-List</h1>
+      <button class="toggle" onClick={() => setShow(!show)}>
+        {show ? "Hide All" : "Show All"}
+      </button>
+      <ul className="List">
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "notcompleted"}
+            >
+              <span className="task id">{task.id}</span>{" "}
+              <span className="task name">{task.name}</span>
+              <span className="task button">
+                <button name="checkbox" class="toggle">
+                  {task.completed ? "Mark Incomplete" : "Mark Complete"}
+                </button>
+              </span>
+            </li>
+          ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="Add new task"
+          name="new-task"
+          id="ntask"
+          required
+          onChange={handleChange}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
